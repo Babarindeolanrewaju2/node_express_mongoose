f02d22d8c2567b4a6bbad764bf3139983dd75e26
Babarindeolanrewaju2 <babarindeolanrewaju@gmail.com>
2023-02-06 17:28:21 +0100

    old pts

--- A Connectivity Diagnoser.py
+++ A Connectivity Diagnoser.py
@@ -2,14 +2,16 @@ import requests
 
 WEBSITES = ["https://www.google.com", "https://www.facebook.com", "https://www.amazon.com"]
 
+output = []
+
 # Try connecting to each website once
 for website in WEBSITES:
     try:
         response = requests.get(website, timeout=5)
         response.raise_for_status()
-        print(f"Successfully connected to {website}")
+        output.append(f"Successfully connected to {website}")
     except requests.exceptions.RequestException as e:
-        print(f"Failed to connect to {website}: {e}")
+        output.append(f"Failed to connect to {website}: {e}")
 
 # check if all of the website could not be connected
 if all(x.startswith("Failed") for x in output):
@@ -22,11 +24,11 @@ else:
                 try:
                     response = requests.get(website, timeout=5)
                     response.raise_for_status()
-                    print(f"Successfully connected to {website}")
+                    output.append(f"Successfully connected to {website}")
                     break
                 except requests.exceptions.RequestException as e:
-                    if i==1:
-                        print(f"{website} is probably offline.")
+                    if i == 1:
+                        output.append(f"{website} is probably offline.")
                     continue
     # checking internet connection based on average of response time
     total_time = 0
@@ -34,9 +36,8 @@ else:
         if "Successfully" in output:
             response = requests.get(website)
             total_time += response.elapsed.total_seconds()
-    avg_time = total_time/len(WEBSITES)
+    avg_time = total_time / len(WEBSITES)
     if avg_time > 1:
         print("The internet connection seems to be slow")
     else:
         print("The internet connection seems to be good")
-
