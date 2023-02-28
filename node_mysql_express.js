43902856a48c4e7e6f400f5d55fe00d6fc8772dd
Babarindeolanrewaju2 <babarindeolanrewaju@gmail.com>
2023-02-28 22:23:24 +0100

    cookies node express mysql async await

--- node_mysql_express.js
+++ node_mysql_express.js
@@ -22,22 +22,15 @@ db.connect((err) => {
 });
 
 // Create table if it does not exist
-const createTableIfNotExists = () => {
-    const sql = `CREATE TABLE IF NOT EXISTS cookies (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    premium BOOLEAN NOT NULL DEFAULT FALSE,
-    offer VARCHAR(255),
-    UNIQUE KEY (name)
-  )`;
-    db.query(sql, (err) => {
-        if (err) {
-            console.log(err);
-            throw err;
-        }
+const createTableIfNotExists = async () => {
+    const sql = `CREATE TABLE IF NOT EXISTS cookies(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL, price DECIMAL(10, 2) NOT NULL, premium BOOLEAN NOT NULL DEFAULT FALSE, offer VARCHAR(255), UNIQUE KEY(name))`;
+    try {
+        const result = await db.promise().query(sql);
         console.log('Table created or already exists');
-    });
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
 };
 
 createTableIfNotExists();
@@ -45,36 +38,36 @@ createTableIfNotExists();
 // Routes
 
 // GET all cookies
-app.get('/cookies', (req, res) => {
+app.get('/cookies', async (req, res) => {
     const sql = 'SELECT * FROM cookies';
-    db.query(sql, (err, result) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send('Internal server error');
-        } else {
-            res.json(result);
-        }
-    });
+    try {
+        const [result, _] = await db.promise().query(sql);
+        res.json(result);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Internal server error');
+    }
 });
 
 // GET a cookie by name
-app.get('/cookies/:name', (req, res) => {
+app.get('/cookies/:name', async (req, res) => {
     const name = req.params.name;
     const sql = 'SELECT * FROM cookies WHERE name = ?';
-    db.query(sql, [name], (err, result) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send('Internal server error');
-        } else if (result.length === 0) {
+    try {
+        const [result, _] = await db.promise().query(sql, [name]);
+        if (result.length === 0) {
             res.status(404).send('Cookie not found');
         } else {
             res.json(result[0]);
         }
-    });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Internal server error');
+    }
 });
 
 // POST a new cookie
-app.post('/cookies', (req, res) => {
+app.post('/cookies', async (req, res) => {
     const {
         name,
         price,
@@ -82,18 +75,17 @@ app.post('/cookies', (req, res) => {
         offer
     } = req.body;
     const sql = 'INSERT INTO cookies (name, price, premium, offer) VALUES (?, ?, ?, ?)';
-    db.query(sql, [name, price, premium, offer], (err, result) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send('Internal server error');
-        } else {
-            res.status(201).send('Cookie created');
-        }
-    });
+    try {
+        const result = await db.promise().query(sql, [name, price, premium, offer]);
+        res.status(201).send('Cookie created');
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Internal server error');
+    }
 });
 
 // PUT (update) a cookie by name
-app.put('/cookies/:name', (req, res) => {
+app.put('/cookies/:name', async (req, res) => {
     const name = req.params.name;
     const {
         price,
@@ -101,32 +93,36 @@ app.put('/cookies/:name', (req, res) => {
         offer
     } = req.body;
     const sql = 'UPDATE cookies SET price = ?, premium = ?, offer = ? WHERE name = ?';
-    db.query(sql, [price, premium, offer, name], (err, result) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send('Internal server error');
-        } else if (result.affectedRows === 0) {
+    try {
+        const result = await db.promise().query(sql, [price, premium, offer, name]);
+        if (result[0].affectedRows === 0) {
             res.status(404).send('Cookie not found');
         } else {
             res.send('Cookie updated');
         }
-    });
-})
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Internal server error');
+    }
+});
 
 // DELETE a cookie by name
-app.delete('/cookies/:name', (req, res) => {
+app.delete('/cookies/:name', async (req, res) => {
     const name = req.params.name;
     const sql = 'DELETE FROM cookies WHERE name = ?';
-    db.query(sql, [name], (err, result) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send('Internal server error');
-        } else if (result.affectedRows === 0) {
+    try {
+        const
+
+            result = await db.promise().query(sql, [name]);
+        if (result[0].affectedRows === 0) {
             res.status(404).send('Cookie not found');
         } else {
             res.send('Cookie deleted');
         }
-    });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal server error');
+    }
 });
 
 // Listen for incoming requests
